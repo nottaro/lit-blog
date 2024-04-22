c1fa5af77057e19eefc911100bf49fda2804f348
nottaro <126886046+nottaro@users.noreply.github.com>
2024-04-22 12:52:12 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-AU",
-    baseUrl: "https://nottaro.github.io/blog/",
+    baseUrl: "nottaro.github.io/blog",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
